882a41b5986b3fd42b8888455e7991e9bdb60f51
Andrey <andre_raz@list.ru>
2024-06-05 13:37:34 +0300

    Bobik

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,26 @@
 const { program } = require('commander');
 
 program
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information')
-  .parse(process.argv);
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+    if (options.format) {
+      console.log(`Output format: ${options.format}`);
+    }
+  });
+
+// Проверка, если нет аргументов или недостаточно аргументов, выводим справку
+if (process.argv.length < 4) {
+  program.help();
+}
+
+program.parse(process.argv);
+
+
+
+
+
+
